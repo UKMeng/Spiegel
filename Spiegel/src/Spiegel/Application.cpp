2c70e74124b5b8813e653b3f517f4835c9257d48
UKMeng <ukmengse@gmail.com>
2024-03-20 18:53:04 +0800

    ✨ feat(Window): Implementation of GLFW callback

--- Spiegel/src/Spiegel/Application.cpp
+++ Spiegel/src/Spiegel/Application.cpp
@@ -2,14 +2,20 @@
 
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
 #include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace spg {
+
+// 回调函数
+// std::placeholders::_1 是一个占位符
+// std::bind，允许您将成员函数绑定到特定对象，并可以将参数传递给该函数。
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
@@ -22,5 +28,17 @@ namespace spg {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		SPG_CORE_TRACE("{0}", e.ToString());
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- Spiegel/src/Spiegel/Application.h
+++ Spiegel/src/Spiegel/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Window.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace spg {
 	class SPG_API Application
@@ -12,7 +13,11 @@ namespace spg {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
