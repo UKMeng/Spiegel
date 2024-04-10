add7901988e7082e9a89b0243f1aa727a426ca60
UKMeng <ukmengse@gmail.com>
2024-04-10 15:47:05 +0800

    ✨ feat(Editor): Properties Panel

--- Spiegel_Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Spiegel_Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,6 +1,7 @@
 #include "SceneHierarchyPanel.h"
 
 #include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace spg {
 
@@ -24,8 +25,20 @@ namespace spg {
 				DrawEntityNode(entity);
 			});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered()) m_SelectionContext = {};
+
+		ImGui::End();
+
+		
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
 		ImGui::End();
 	}
+
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
 	{
 		auto& tag = entity.GetComponent<TagComponent>().Tag;
@@ -36,5 +49,43 @@ namespace spg {
 			m_SelectionContext = entity;
 
 		if (opened) ImGui::TreePop();
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.CheckComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.CheckComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& tc = entity.GetComponent<TransformComponent>();
+				ImGui::DragFloat3("Position", glm::value_ptr(tc.Transform[3]), 0.1f);
+				/*ImGui::DragFloat3("Rotation", glm::value_ptr(tc.Rotation));
+				ImGui::DragFloat3("Scale", glm::value_ptr(tc.Scale))*/;
+				ImGui::TreePop();
+			}
+		}
+
+		//if (entity.CheckComponent<SpriteRendererComponent>())
+		//{
+		//	if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer"))
+		//	{
+		//		auto& spriteRenderer = entity.GetComponent<SpriteRendererComponent>();
+		//		ImGui::ColorEdit4("Color", glm::value_ptr(spriteRenderer.Color));
+		//		ImGui::TreePop();
+		//	}
+		//}
 	}
 }
